778b2cb66daaa44e9e2eb2aa586ad7daa870bcde
Rob <robcalcote@gmail.com>
2018-10-28 15:18:51 -0600

    Added required status to all data fields of the RSVP form as well as messages if the user didn't fill out a field

--- Views/ViewsPractice/ViewsPractice/Models/GuestResponses.cs
+++ Views/ViewsPractice/ViewsPractice/Models/GuestResponses.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,16 @@ namespace ViewsPractice.Models
 {
     public class GuestResponses
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string sName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
         public string sEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
         public string sPhone { get; set; }
+
+        [Required(ErrorMessage = "Please specify if you will attend")]
         // ? on the end of bool allows you to store, true, false or null.
         public bool? bWillAttend { get; set; }
     }

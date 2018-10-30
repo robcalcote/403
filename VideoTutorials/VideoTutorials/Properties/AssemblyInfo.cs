02d5793293e5e85536f85296b9fbc52d172bfe6a
Rob <robcalcote@gmail.com>
2018-10-29 22:29:46 -0600

    changed assembly info to include AssemblyDescription

--- VideoTutorials/VideoTutorials/Properties/AssemblyInfo.cs
+++ VideoTutorials/VideoTutorials/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VideoTutorials")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This project is on Github.com/robcalcote/403 - it is all of the video tutorials")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VideoTutorials")]

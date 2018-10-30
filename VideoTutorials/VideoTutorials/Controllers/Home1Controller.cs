561a87014a4243ea88dad3a6e296ac31eec44665
Rob <robcalcote@gmail.com>
2018-10-29 22:56:13 -0600

    Halfway through github assignment

---
+++ VideoTutorials/VideoTutorials/Controllers/Home1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// Controller for ASP.NET MVC C# Tutorial video assignment
+
+namespace VideoTutorials.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        // GET: Home1
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+
+            String sTeamName = "Gonzaga";
+
+            ViewBag.Opponent = sTeamName.Equals("Gonzaga") ? "Beat Gonzaga" : "Beat anyone else";
+
+            return View();
+        }
+    }
+}

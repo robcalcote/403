8010337f40a47553ba3ee1ff981667e5395e0f5e
Rob <robcalcote@gmail.com>
2018-11-04 14:35:46 -0700

    Various small changes

---
+++ VideoTutorials/VideoTutorials/Controllers/Home0912Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VideoTutorials.Controllers
+{
+    public class Home0912Controller : Controller
+    {
+        // GET: Home0912
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

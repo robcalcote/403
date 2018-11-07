369eca9cd5c8e5ebee75577be8a69f1a16304532
Rob <robcalcote@gmail.com>
2018-11-07 08:38:00 -0700

    Added a new Model, View, and Controller for Sep 19 YouTube tutorial.

---
+++ VideoTutorials/VideoTutorials/Models/Sep19/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoTutorials.Models.Sep19
+{
+    public class Team
+    {
+        // Added Soccer team
+        public string teamName { get; set; }
+        public int teamPoints { get; set; }
+
+
+
+        // THIS DOES THE SAME THING AS ABOVE
+        //private string teamName;
+
+        //public string TeamName
+        //{
+        //    get { return teamName; }
+        //    set { teamName = value; }
+        //}
+
+    }
+}
